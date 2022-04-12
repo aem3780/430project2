f8e623c9e166ae9847317140d46d3c485059e2ec
Allie Maus <aem3780@rit.edu>
2022-04-12 14:00:36 -0400

    cloning over domo maker repo

---
+++ client/helper.js
@@ -0,0 +1,67 @@
+
+const handleError = (message) => {
+  document.getElementById('errorMessage').textContent = message;
+  document.getElementById('domoMessage').classList.remove('hidden');
+};
+
+const sendPost = async (url, data, handler) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+
+  const result = await response.json();
+  document.getElementById('domoMessage').classList.add('hidden');
+
+  if(result.error) {
+      handleError(result.error);
+  }
+
+  if(result.redirect) {
+      window.location = result.redirect;
+  }
+
+  if(handler) {
+      handler(result);
+  }
+};
+
+const hideError = () => {
+    document.getElementById('domoMessage').classList.add('hidden');
+};
+
+
+const domoDelete = async (url, data, handler) => {
+  const response = await fetch(url, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+      body: JSON.stringify(data),
+    
+  });
+
+  const result = await response.json();
+
+  if(result.error) {
+      handleError(result.error);
+  }
+
+  if(result.redirect) {
+      window.location = result.redirect;
+  }
+
+  if(handler) {
+      handler(result);
+  }
+};
+
+module.exports = {
+    handleError,
+    sendPost,
+    hideError,
+    domoDelete,
+};
